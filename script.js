e9e7882de682429f60f45d64ff081ff0db66cebe
itsrahul_8 <149181581+rahuldk1105@users.noreply.github.com>
2024-01-04 23:17:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,15 @@ let preValue;
 generateBtn.addEventListener("click", () => {
   let qrValue = qrInput.value.trim();
   if (!qrValue || preValue === qrValue) return;
+
+  console.log("Generating QR Code...");
   preValue = qrValue;
   generateBtn.innerText = "Generating QR Code...";
+  
   qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+
   qrImg.addEventListener("load", () => {
+    console.log("QR Code loaded successfully");
     wrapper.classList.add("active");
     generateBtn.innerText = "Generate QR Code";
   });
@@ -18,8 +23,8 @@ generateBtn.addEventListener("click", () => {
 
 qrInput.addEventListener("keyup", () => {
   if (!qrInput.value.trim()) {
+    console.log("Input value is empty");
     wrapper.classList.remove("active");
     preValue = "";
   }
 });
-  
